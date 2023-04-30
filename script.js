121049c7a04ae391fd5c98e7a796fe400c0b4f26
Winfrey De Vera <w.jamesdevera@gmail.com>
2023-04-30 21:51:30 +0800

    added commas on numbers and fixed text output

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const DEFAULT_TEXT_OUTPUT_FONT_SIZE = 96
+
+
 const textOutput = document.getElementById('output');
 const numberButtons = document.querySelectorAll('#number');
 const clearButton = document.getElementById('clear');
@@ -5,16 +8,26 @@ const clearButton = document.getElementById('clear');
 clearButton.onclick = () => clearAll();
 
 function updateTextOutput() {
+    const textOutputSize = textOutput.textContent.length;
+    let text = textOutput.textContent.replace(/\W/g, '');
+    console.log(text);
     if (textOutput.textContent === "0") {
-        textOutput.textContent = this.value; 
-    } else {
-        textOutput.textContent += this.value;
+        textOutput.textContent = this.value;
+    } else if (textOutputSize < 11) {
+        text += this.value;
+        textOutput.textContent = numberWithCommas(text);
     }
+    // console.log(textOutputSize);
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 function clearAll() {
     textOutput.textContent = '0';
 }
+
 numberButtons.forEach(numberButton => {
     numberButton.addEventListener('click', updateTextOutput);
 })
