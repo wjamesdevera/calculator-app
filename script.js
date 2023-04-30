60e0049701b5d6450bdeae457765c9a8f6474c00
Winfrey De Vera <w.jamesdevera@gmail.com>
2023-04-30 23:57:37 +0800

    added operation functionality:

--- script.js
+++ script.js
@@ -5,16 +5,20 @@ const textOutput = document.getElementById('output');
 const numberButtons = document.querySelectorAll('#number');
 const clearButton = document.getElementById('clear');
 const operationBtns = document.querySelectorAll('.operation');
+const equalBtn = document.getElementById('equal');
 
 
 let currentOperation = '';
 let operationOn = false;
+let firstValue = 0;
+let nextValue = 0;
 
 clearButton.onclick = () => clearAll();
 
 function updateTextOutput() {
     const textOutputSize = textOutput.textContent.length;
     let text = textOutput.textContent.replace(/,/g, '');
+    checkClearCheckBox();
     console.log(text);
     if (textOutput.textContent === "0") {
         textOutput.textContent = this.value;
@@ -32,6 +36,30 @@ function clearAll() {
     textOutput.textContent = '0';
 }
 
+function checkClearCheckBox() {
+    if (operationOn) {
+        firstValue = textOutput.textContent.replace(/,/g, '');
+        textOutput.textContent = '0';
+        operationOn = false;
+    }
+}
+
+function clearPressedOperation() {
+    operationBtns.forEach(operationBtn => {
+        operationBtn.classList.remove('orange-pressed');
+    })
+}
+
+function checkOperation() {
+    let isOn = false;
+    operationBtns.forEach(operationBtn => {
+        if (operationBtn.classList.contains('orange-pressed')) {
+            isOn = true;
+        }
+    })
+    operationOn = isOn;
+}
+
 numberButtons.forEach(numberButton => {
     numberButton.addEventListener('click', updateTextOutput);
 })
@@ -45,6 +73,56 @@ operationBtns.forEach(operationBtn => {
         })
         operationBtn.classList.toggle('orange-pressed');
         currentOperation = operationBtn.value;
-        console.log(currentOperation);
+        checkOperation();
     })
 })
+
+
+function compute() {
+    checkOperation();
+    console.log(operationOn);
+    let result = 0;
+    if (!operationOn) {
+        return;
+    }
+    clearPressedOperation();
+
+    nextValue = textOutput.textContent.replace(/,/g, '');
+
+    switch(currentOperation) {
+        case '+':
+            result = add(firstValue, nextValue);
+            break;
+        case '-':
+            result = subtract(firstValue, nextValue);
+            break;
+        case '*':
+            result = multiply(firstValue, nextValue);
+            break;
+        case '/':
+            result = divide(firstValue, nextValue);
+            break;
+        default:
+            break;
+    }
+    firstValue = result;
+    textOutput.textContent = result;
+}
+
+equalBtn.addEventListener('click', () => {compute()});
+
+function add(a, b) {
+    return Number(a) + Number(b);
+}
+
+function subtract(a, b) {
+    return Number(a) - Number(b);
+}
+
+function multiply(a, b) {
+    return Number(a) * Number(b);
+}
+
+function divide(a, b) {
+    return Number(a) / Number(b);
+}
